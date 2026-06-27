6bfe2820fed49656a999a331ec4c5db367dee37e
WiCONTABLE <wicontable@gmail.com>
2026-06-27 12:34:41 -0500

    productos.js

--- productos.js
+++ productos.js
@@ -8,7 +8,7 @@ const productos = [
         mostrarTecnica: true,
 etiqueta: "ACTIVA",
         desc: "<b>REF006</b> <p>Accede y descarga cualquier documento electrónico en PDF directamente desde la DIAN de forma rápida, sencilla y sin procesos manuales innecesarios.<p><b>Actualizada el 28-12-2025</b>",
-        caracteristicas: ["No necesitas generar TOKEN, ni Certificado Digital DIAN para para generar la descarga PDF.","Carga un archivo EXCEL ó pega directamente los CUDE, CUDS, CUFE, CUNE.", "Procesa en tiempo de respuesta según captcha de 3, 6, 9 segundos por documento.", "Extra aplicativo (WiPDFs) para desbloquear vista previa, renombrar nómina y listar archivos."],
+        caracteristicas: ["No necesitas generar TOKEN, ni Certificado Digital DIAN para la descarga de PDFs.","Carga un archivo EXCEL ó pega directamente los CUDE, CUDS, CUFE, CUNE.", "Procesa en tiempo de respuesta según captcha de 3, 6, 9 segundos por documento.", "Extra aplicativo (WiPDFs) para desbloquear vista previa, renombrar nómina y listar archivos."],
         nota: "Para brindarle un soporte técnico oportuno, requerimos conectarnos de forma remota a su equipo a través de AnyDesk. Tenga en cuenta que este acceso quedará registrado de manera individual en cada computadora durante la instalación. Por su seguridad, nuestro equipo se comunicará exclusivamente al número de móvil que tiene registrado con nosotros. Asimismo, le recordamos que el funcionamiento de nuestra herramienta depende de plataformas de terceros integradas; cualquier cambio en los parámetros de dichas plataformas nos exime de responsabilidad sobre la continuidad del servicio.</b>",
         precio: "COP $30.000",
         video: "https://www.youtube.com/watch?v=hiZn2nFgtX4&t",

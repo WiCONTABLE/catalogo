9e637c5a3fe2c3233578f3405cc445e36f327153
WiCONTABLE <wicontable@gmail.com>
2026-07-18 20:05:00 -0500

    productos.js

--- productos.js
+++ productos.js
@@ -14,7 +14,7 @@ const productos = [
         precio: "COP $5.000",
         video: "video/ReportePN.mp4",
         youtube: "https://www.youtube.com/watch?v=tWS24jBXEkc",
-        fotos: ["img/p12-0.png", "img/p12-1.jpg"]
+        fotos: ["img/p12-0.png", "img/p12-1.jpg", "img/p12-2.jpg", "img/p12-3.jpg"]
     },       
         {
         id: 0,

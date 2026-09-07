efecb1100da55b729939cef5f6be778b58329f0f
WiCONTABLE <wicontable@gmail.com>
2026-09-07 15:02:49 -0500

    productos.js

--- productos.js
+++ productos.js
@@ -31,8 +31,7 @@ const productos = [
         youtube: "https://youtu.be/qa_wJjN3Nvo",
         fotos: ["img/p12-0.png", "img/p12-1.png", "img/p12-2.png", "img/p12-3.png"]
     },     
-            
-    {
+ {
         id: 3,
         nombre: "Consulta Datos NIT (DIAN)",
         url: "consulta-datos-nit.html",
@@ -130,7 +129,7 @@ etiqueta: "DISPONIBLE",
         url: "spotify3meses.html",
         formato: "Credenciales",
         mostrarTecnica: true,
-        etiqueta: "NUEVO",
+        etiqueta: "DISPONIBLE",
         desc: "<p>Recibe una cuenta Premium lista para usar, con su respectivo correo y contraseña.</p><p>¿Ya tienes una cuenta gratuita con tus playlists favoritas? ¡No las pierdes! Comparte el enlace de tus listas con la cuenta Premium, guárdalas allí y disfruta durante 3 meses premium; La oferta termina el 23 de septiembre de 2026.</p>",
         caracteristicas: ["<p>Música sin anuncios.</p>","<p>Reproducción de cualquier canción.</p>", "<p>Cambios de canción sin límites.</p>", "<p>Descargas para escuchar sin internet.</p>", "<p>Excelente calidad de audio</p>"],
         nota: "<b><p>RECUERDE</b> que es importante ver el videotutorial antes de adquirir la credencial, de esta manera le será más fácil despejar dudas de funcionamiento.</p> <p>Las credenciales toman un tiempo 15 minuto para la entrega una vez hayas realizado el respectivo pago.</p>",

5492c7133a6c1593c5bf54b63cc470922ef42cb8
WiCONTABLE <wicontable@gmail.com>
2026-09-07 14:58:53 -0500

    productos.js

--- productos.js
+++ productos.js
@@ -1,21 +1,6 @@
 const miTelefono = "573156016386";
 
 const productos = [
-        {
-        id: 1,
-        nombre: "Spotify (3 Meses)",
-        url: "spotify3meses.html",
-        formato: "Credenciales",
-        mostrarTecnica: true,
-        etiqueta: "NUEVO",
-        desc: "<p>Recibe una cuenta Premium lista para usar, con su respectivo correo y contraseña.</p><p>¿Ya tienes una cuenta gratuita con tus playlists favoritas? ¡No las pierdes! Comparte el enlace de tus listas con la cuenta Premium, guárdalas allí y disfruta durante 3 meses premium; La oferta termina el 23 de septiembre de 2026.</p>",
-        caracteristicas: ["<p>Música sin anuncios.</p>","<p>Reproducción de cualquier canción.</p>", "<p>Cambios de canción sin límites.</p>", "<p>Descargas para escuchar sin internet.</p>", "<p>Excelente calidad de audio</p>"],
-        nota: "<b><p>RECUERDE</b> que es importante ver el videotutorial antes de adquirir la credencial, de esta manera le será más fácil despejar dudas de funcionamiento.</p> <p>Las credenciales toman un tiempo 15 minuto para la entrega una vez hayas realizado el respectivo pago.</p>",
-        precio: "COP $5.000",
-        video: "video/Spotify.mp4",
-        youtube: "",
-        fotos: ["img/p1-0.png", "img/p1-1.png", "img/p1-2.png"]
-    },
     {
         id: 2,
         nombre: "PDFs Downloader (DIAN)",
@@ -138,6 +123,21 @@ etiqueta: "DISPONIBLE",
         precio: "Desde COP $1.500",
         video: "https://acortar.link/Plataformax",
         fotos: ["img/p10-0.png"]
+    },
+{
+        id: 1,
+        nombre: "Spotify (3 Meses)",
+        url: "spotify3meses.html",
+        formato: "Credenciales",
+        mostrarTecnica: true,
+        etiqueta: "NUEVO",
+        desc: "<p>Recibe una cuenta Premium lista para usar, con su respectivo correo y contraseña.</p><p>¿Ya tienes una cuenta gratuita con tus playlists favoritas? ¡No las pierdes! Comparte el enlace de tus listas con la cuenta Premium, guárdalas allí y disfruta durante 3 meses premium; La oferta termina el 23 de septiembre de 2026.</p>",
+        caracteristicas: ["<p>Música sin anuncios.</p>","<p>Reproducción de cualquier canción.</p>", "<p>Cambios de canción sin límites.</p>", "<p>Descargas para escuchar sin internet.</p>", "<p>Excelente calidad de audio</p>"],
+        nota: "<b><p>RECUERDE</b> que es importante ver el videotutorial antes de adquirir la credencial, de esta manera le será más fácil despejar dudas de funcionamiento.</p> <p>Las credenciales toman un tiempo 15 minuto para la entrega una vez hayas realizado el respectivo pago.</p>",
+        precio: "COP $5.000",
+        video: "video/Spotify.mp4",
+        youtube: "",
+        fotos: ["img/p1-0.png", "img/p1-1.png", "img/p1-2.png"]
     },
 {
         id: 11,
